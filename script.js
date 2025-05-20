853d4daf5a959270050d3f790012aa57adda6335
Komal Sasane <sasanekomal11@gmail.com>
2025-05-20 16:47:22 +0530

    Add welcome message in JS

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // Example: Display a welcome message when the page loads
-window.onload = function () {
-    alert("Welcome to my portfolio!");
-};
-
-
-
+window.addEventListener('load', () => {
+  alert("Welcome to my portfolio!");
+});
+;
